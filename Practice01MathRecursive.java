7dbc31046a2e4adc3057529a4105f13ee2f38a33
yelangzhidi <yelangzhidi@gmail.com>
2019-08-23 14:58:37 -0700

    Update Practice01MathRecursive.java

--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -1,3 +1,14 @@
 public class Practice01MathRecursive {
-
+  
+  public int fib(int n) {
+    if (n == 0 || n == 1)
+      return n;
+    return fib(n-1) + fib(n-2);
+  }
+  public int fact(int n) {
+    if (n == 1)
+      return 1;
+    return fact(n-1);
+  }
+  
 }
